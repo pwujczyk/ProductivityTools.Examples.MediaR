a93f249826d8881abbb7569087257de61ff70f13
pwujczyk <pwujczyk@ecovadis.com>
2019-10-18 13:25:08 +0200

    test command

---
+++ ProductivityTools.Examples.MediaR/Controllers/MediaRController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductivityTools.Examples.MediaR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaRController : ControllerBase
+    {
+        [HttpGet("Test")]
+        public ActionResult<string> Test()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
--- ProductivityTools.Examples.MediaR/Startup.cs
+++ ProductivityTools.Examples.MediaR/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,7 @@ namespace ProductivityTools.Examples.MediaR
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMediatR(System.Reflection.Assembly.GetAssembly(this.GetType()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
